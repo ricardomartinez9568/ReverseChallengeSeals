4006622e40a42ff535df57b830c689443637bb73
Ricky Martinez <rm3@mscpsharks.org>
2018-01-31 23:08:16 -0800

    2nd commit

--- ReverseThis.js
+++ ReverseThis.js
@@ -1,9 +1,8 @@
-function reverse(s) {
-    var s = document.getElementById("reverse").value;
-    var o = [];
-    for (var i = s.length - 1, j = 0; i >= 0; i--, j++) {
-      o[j] = s[i];
-    return o.join('');
-    }
-    console.log(reverse)
+function reverse() {
+  var s = document.getElementById("reverse").value;
+  var o = [];
+  for (var i = s.length - 1; i >= 0; i--) {
+    o += s[i];
+  console.log(o);
+  }
 }
